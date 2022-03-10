121851f3c2b3df56cf8b453d2f470a2ac05a8eda
Darlley <systembrtt@gmail.com>
2022-03-10 08:45:32 -0400

    ok

--- aulas/46.php
+++ aulas/46.php
@@ -1,45 +1,25 @@
-<?php // Encapsulamento
+<?php
 
-    /*
-    As 3 formas de caessar atributos e metodos:
-    Atributose métodos da mesma classe
-    Atributos e metodos das classes extendiadas
-    Atributos e métodos acessados por objeto     
+    class Livro {
+        protected $isbn = '8575223720';
+        public $name = "JavaScrip Remoto";
+        private $id_amazon = 'A1ZZFT5FULY4LN';
 
-    */
-
-    class Pessoa {
-
-        public $nome = "Darlley"; // public todo mundo vê
-        protected $idade = 23; // protected é visto pelos da mesma classe e classe extendida
-        private $senha = 'dev123'; // private só é visto pela mesma classe
-
-        public function mostrar(){
-            // metodo da mesma classe que os atributos então enxerga tudo
-            echo $this->nome . '<br>';
-            echo $this->idade . '<br>';
-            echo $this->senha . '<br>';
-        }
-
-    }
-
-    
-    // EXEMPLO 2
-    class Programador extends Pessoa {
-        // A classe Programador extende tudo da classe Pessoa, exceto os atributos e métodos privados
-        
-        public function mostrar(){
-            
-            echo get_class($this) . '<br>';
-            
-            echo $this->nome . '<br>';
-            echo $this->idade . '<br>';
-            echo $this->senha . '<br>';
+        public function mostrar_amazon(){
+            echo $this->name . '<br>';
+            echo $this->isbn . '<br>';
+            echo $this->id_amazon . '<br>';
         }
     }
 
-    $estudante = new Programador();
-    echo $estudante->nome . '<br>';
-    // echo $estudante->idade . '<br>';
-    // echo $estudante->senha . '<br>';
-    echo $estudante->mostrar();
+
+    class Kindle extends Livro{
+        public function mostrar_shopping(){
+            echo $this->name . '<br>';
+            echo $this->isbn . '<br>';
+            echo $this->id_amazon . '<br>';
+        }
+    }
+
+    echo (new Livro())->mostrar_amazon() . '<br>';
+    echo (new Kindle())->mostrar_shopping();
