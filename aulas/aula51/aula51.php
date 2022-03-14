b192d27359d354f71996dc960ab7fbf6d694ecd1
Darlley <systembrtt@gmail.com>
2022-03-14 14:23:49 -0400

    aula 51 2/2

--- aulas/aula51/aula51.php
+++ aulas/aula51/aula51.php
@@ -2,12 +2,15 @@
 
     // Metodo Mágico Autoload
     // __autoload esta OBSOLETA A PARTIRDO PHP8
+    // para PHP8 use spl_autoload_register
 
     spl_autoload_register(
         function($class){
 
-            if (file_exists($class.".php")) {
-                require_once($class.".php");
+            if (file_exists("abstract". DIRECTORY_SEPARATOR . "$class.php")) {
+                require_once("abstract".DIRECTORY_SEPARATOR."$class.php");
+            }else{
+                require_once("$class.php");
             }
             
         }
