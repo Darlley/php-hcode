b3b68ea9c421e044c34f902e204a09a42193f438
Darlley <systembrtt@gmail.com>
2022-03-18 10:00:56 -0400

    aula 64

---
+++ aulas/DAO/index.php
@@ -0,0 +1,6 @@
+<?php
+
+    require_once('config.php');
+    $sql = new Sql('dev_php7','root','123@ldz'); // Databse name, user, password
+    $usuarios = $sql->select("SELECT * FROM tb_usuarios");
+    echo json_encode($usuarios); 
