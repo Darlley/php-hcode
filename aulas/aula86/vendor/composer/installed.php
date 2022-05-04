53830fe265d1a2d73aeed4abd47c9fe6e139293a
Darlley <systembrtt@gmail.com>
2022-05-04 08:48:29 -0400

    aula86

---
+++ aulas/aula86/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'c41205c572ae904b340ccd0d8e99e3be9d984ef1',
+        'name' => 'hcode/composer',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hcode/composer' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'c41205c572ae904b340ccd0d8e99e3be9d984ef1',
+            'dev_requirement' => false,
+        ),
+        'monolog/monolog' => array(
+            'pretty_version' => '1.0.2',
+            'version' => '1.0.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../monolog/monolog',
+            'aliases' => array(),
+            'reference' => 'b704c49a3051536f67f2d39f13568f74615b9922',
+            'dev_requirement' => false,
+        ),
+    ),
+);
