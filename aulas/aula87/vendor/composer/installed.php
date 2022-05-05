98fc716449a174cb3e85c93cc7afa0bc699bab5d
Darlley <systembrtt@gmail.com>
2022-05-05 08:09:20 -0400

    aula87

---
+++ aulas/aula87/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '53830fe265d1a2d73aeed4abd47c9fe6e139293a',
+        'name' => 'hcode/phpmailer',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hcode/phpmailer' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '53830fe265d1a2d73aeed4abd47c9fe6e139293a',
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.6.0',
+            'version' => '6.6.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'reference' => 'e43bac82edc26ca04b36143a48bde1c051cfd5b1',
+            'dev_requirement' => false,
+        ),
+    ),
+);
